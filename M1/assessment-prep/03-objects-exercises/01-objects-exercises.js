c62358dad36a156aa98094f9a78ac2bd2d2c2216
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-02-16 10:12:09 -0500

    added object exercises

---
+++ M1/assessment-prep/03-objects-exercises/01-objects-exercises.js
@@ -0,0 +1,113 @@
+/**
+ * 1
+ * Write a function to return the keys of a  object.
+ *
+ * example output: name,sclass,rollno
+ */
+const student = {
+  name: 'David Rayy',
+  sclass: 'VI',
+  rollno: 12,
+};
+
+/**
+ * 2
+ * Write a function to delete the rollno property from the following object.
+ * Print the object before AND after deleting the property.
+ */
+
+/**
+ * 3
+ *
+ * Write a function to get the length of a  object.
+ */
+
+/**
+ * 4
+ *
+ * Write a  function to print the data or each book on a single line (i.e. display book name, author name and reading status) of the following books.
+ */
+
+const library = [
+  {
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true,
+  },
+  {
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true,
+  },
+  {
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false,
+  },
+];
+
+/**
+ * 5
+ *
+ * Write a function given the radius of a circle
+ * to calculate and return the area and perimeter of a circle.
+ *
+ */
+
+/**
+ * 6
+ * 
+ * Write a  function to return a sorted array of objects by the libraryID
+ * 
+ * example output:
+    [
+      {
+        author: "Bill Gates",
+        libraryID: 1254,
+        title: "The Road Ahead"
+      },
+      {
+        author: "Suzanne Collins",
+        libraryID: 3245,
+        title: "Mockingjay: The Final Book of The Hunger Games"
+      },
+      {
+        author: "Walter Isaacson",
+        libraryID: 4264,
+        title: "Steve Jobs"
+      },
+    ]
+*/
+
+/**
+ * 7
+ *
+ * Write a  function to convert an object into a list of `[key, value]` pairs.
+ */
+
+const book = {
+  author: 'Bill Gates',
+  libraryID: 1254,
+  title: 'The Road Ahead',
+};
+
+/**
+ * 8
+ * 
+ * Write a  function to get a copy of the object
+ * where the keys have become the values and the values the keys.
+ * 
+ * example output:
+  {
+    bill_gates: "author",
+    1254: "libraryID",
+    the_road_ahead: "title"
+  };
+ */
+
+/**
+ * 9
+ *
+ * Write a  function to check whether an object contains given property.
+ *
+ */
