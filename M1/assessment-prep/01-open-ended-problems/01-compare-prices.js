76d2f6cb16a597a9ed019a2ed75a488b43c37e4c
named.josie <named.josie@gmail.com>
2022-03-01 10:16:00 -0500

    done

--- M1/assessment-prep/01-open-ended-problems/01-compare-prices.js
+++ M1/assessment-prep/01-open-ended-problems/01-compare-prices.js
@@ -10,8 +10,46 @@
 let priceOfHouse = "€1.500.000";
 let priceOfCar = "€20.000";
 
-if (priceOfHouse > priceOfCar) {
-  console.log("The house is more expensive");
-} else {
-  console.log("The car is more expensive");
+// function commparePrice(){
+
+// if (Number(priceOfHouse) > Number(priceOfCar)) {
+//   return "The house is more expensive";
+// } else {
+//   return "The car is more expensive";
+// }
+// }
+// console.log(commparePrice(priceOfHouse, priceOfCar))
+
+
+
+function commparePrices(price1, price2){
+  result = `Neither price 1: ${price1} or price 2: ${price2} is valid`
+  const strPrices = [price1, price2]
+let strPriceData = {}
+  for (let i =0; i< strPrices.length; i ++){
+    strPriceData[i]= {
+      strPrices[i]: strPrices[i],
+      isString: false,
+      containsNumber: false
+    }
+if(typeof strPrices === 'string'){
+  strPriceData[i].isString = true   
+  
+  for(let char of strPrices[i] )
+ 
+      let tempChar= praseInt(char)
+  if (isNaN(tempChar)){
+    strPriceData[i].containsNumber = true
 }
+  
+    }
+  } if (strPriceData[0].containsNumber === false &&
+    strPriceData[1].containsNumber=== false)
+    {
+    return `${strPriceData[1]} is more expensive that ${strPriceData[0]}`
+  } else if (strPriceData[1].containsNumber === false) {
+    return 
+  }
+  return result;
+}
+
