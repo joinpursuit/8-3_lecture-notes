89913981380d9d0e5cb9640c737f5df911fc9696
named.josie <named.josie@gmail.com>
2022-03-03 16:24:00 -0500

    done with some

--- M2/higher-order-fn/higher-order-fn_exercises_part-1.js
+++ M2/higher-order-fn/higher-order-fn_exercises_part-1.js
@@ -2,7 +2,7 @@
  * Take a look at the functions below. Are any of the functions a higher order function?
  * If so, which ones and why so? If not, why not?
  */
-
+// Yes, there is a higher order function...because the function falsy is incased
 function truthy() {
   return true;
 }
@@ -17,7 +17,7 @@ falsy(); //> false
  * Take a look at the functions below. Are any of the functions a higher order function?
  * If so, which ones and why so? If not, why not?
  */
-
+// the last one is the higher function it calls on both right and left functions...
 function left() {
   return 'Left';
 }
@@ -34,6 +34,7 @@ function go(goLeft = true) {
  * Take a look at the functions below. Are any of the functions a higher order function?
  * If so, which ones and why so? If not, why not?
  */
+//I believe there is no higher functions involved...
 
 const directions = [];
 
@@ -56,14 +57,28 @@ console.log(directions); //> ["Left", "Right", "Up"]
  * The transform() function expects, as its second argument, a function that accepts at least one argument.
  */
 
-function transform(text, fn) {
+function transform(text, fn, ) {
   if (typeof fn !== 'function') {
     return text;
   }
 
   return fn(text);
+
+() {
+  return 'LEFT';
 }
 
+return right() {
+  return 'right';
+}
+
+leftrightleft() {
+return "left-right-left"
+}
+upDownleft () {
+  return 'UDL'
+}
+}
 /**
  * Use the function above to transform the text in the following ways. For each line below, you will need to write an anonymous function.
  *
