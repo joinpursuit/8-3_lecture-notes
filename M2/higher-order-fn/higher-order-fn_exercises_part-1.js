e181697fbb1c4286af86d636a56a1c612e778523
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-03-03 13:39:50 -0500

    added more examples for higher order functions

--- M2/higher-order-fn/higher-order-fn_exercises_part-1.js
+++ M2/higher-order-fn/higher-order-fn_exercises_part-1.js
@@ -81,3 +81,69 @@ transform('hello', function () {});
  * "left right left" -> "left-right-left"
  * "up Down left" -> "UDL"
  */
+
+/**
+ * Write a fn called each which accepts two parameters:
+ *  - an array
+ *  - a callback fn
+ * The each fn loops through the given array and runs the callback function on each of it's elements.
+ */
+function each(arr, fn) {
+  // write your code here
+}
+
+// The following call to each should print
+// 1
+// 2
+// 3
+// 4
+each([1, 2, 3, 4], function (value) {
+  console.log(value);
+});
+
+// The following call to each should print
+// 2
+// 4
+// 6
+// 8
+each([1, 2, 3, 4], function (value) {
+  console.log(value * 2);
+});
+
+/**
+ * Write a fn called map which accepts two parameters:
+ *  - an array
+ *  - a callback fn
+ * The map fn should return a new array where each new element is the return value from the anonymous function with the argument as the original element value.
+ */
+function map(arr, fn) {
+  // write your code here
+}
+
+// The following call to map should return
+// [2, 4, 6, 8];
+map([1, 2, 3, 4], function (value) {
+  return value * 2;
+});
+
+/**
+ * Write a fn called reject which accepts two parameters:
+ *  - an array
+ *  - a callback fn
+ * The reject fn should return a new array containing all of the values that do not return true to the callback
+ */
+function reject(arr, fn) {
+  // write your code here
+}
+
+// The following call to reject should return
+// [1, 2];
+reject([1, 2, 3, 4], function (value) {
+  return value > 2;
+});
+
+// The following call to reject should return
+// [3, 5];
+reject([2, 3, 4, 5], function (value) {
+  return value % 2 === 0;
+});
