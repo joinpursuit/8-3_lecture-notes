3694e7ae4bb875b6c3fbb2959c82e58b03fdfa1c
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-03-07 11:17:48 -0500

    added .forEach notes

--- M2/higher-order-native-array-methods/for-each.js
+++ M2/higher-order-native-array-methods/for-each.js
@@ -1,4 +1,56 @@
 /**
+ * Array.forEach(fn) //<- forEach is a method (property) of the Array object/class
+ * Higher Order because their is a required function parameter
+ *
+ * Side Effects: uses side effects to acheive logical goals/outputs
+ *
+ * Return Values: does NOT make use of return values
+ * - you CANNOT store a call to Array.forEach in a variable
+ * - it returns undefined
+ *
+ * With Array.forEach, we create new Array/Object ONLY -- because there is no return value
+ * non-destructive
+ */
+
+let soccerTeams = [
+  'Manchester United',
+  'Real Madrid',
+  'Boca Juniors',
+  'NYC FC',
+];
+
+function printTeamSupport(team) {
+  return `I am a super fan of ${team}: this is the named function`;
+}
+
+// with vanilla js we can run a loop, calling the printTeamSupport(element)
+
+// With Array.forEach(fn) we can do this
+// returns undefined even though printTeamSupport has a return value
+// because forEach explicitly returns undefined
+console.log(soccerTeams.forEach(printTeamSupport));
+
+soccerTeams.forEach(function (team) {
+  console.log(`I am a super fan of ${team}: this is the anonymous function`);
+});
+
+// create a new array with each team having it's own object
+let soccerTeamsV2 = [];
+
+soccerTeams.forEach(function (team, index) {
+  // let teamObj = {
+  //   name: team,
+  //   fanMessage: printTeamSupport(team);
+  // }
+  // soccerTeamsV2.push(teamObj);
+  soccerTeamsV2[index] = {};
+  soccerTeamsV2[index].name = team;
+  soccerTeamsV2[index].fanMessage = printTeamSupport(team);
+});
+
+console.log(soccerTeamsV2);
+
+/**
  * 1
  *
  * The .forEach() method's callback function has up to three parameters. What are these parameters?
@@ -11,21 +63,21 @@
  */
 
 // do not edit the comics object
-const comics = [
-  { title: 'Calvin & Hobbes', author: 'Bill Watterson', kind: 'print' },
-  { title: 'Zen Pencils', author: 'Gavin Aung Than', kind: 'web' },
-  { title: 'Nancy', author: 'Olivia James', kind: 'print' },
-  { title: 'False Knees', author: 'Joshua Barkman', kind: 'web' },
-];
+// const comics = [
+//   { title: 'Calvin & Hobbes', author: 'Bill Watterson', kind: 'print' },
+//   { title: 'Zen Pencils', author: 'Gavin Aung Than', kind: 'web' },
+//   { title: 'Nancy', author: 'Olivia James', kind: 'print' },
+//   { title: 'False Knees', author: 'Joshua Barkman', kind: 'web' },
+// ];
 
-function printAllComics(comics) {
-  // Write your code here.
-  for (let comic of comics) {
-    console.log(`${comic.title} by ${comic.author}`);
-  }
-}
+// function printAllComics(comics) {
+//   // Write your code here.
+//   for (let comic of comics) {
+//     console.log(`${comic.title} by ${comic.author}`);
+//   }
+// }
 
-printAllComics(comics);
+// printAllComics(comics);
 
 /**
  * 3
