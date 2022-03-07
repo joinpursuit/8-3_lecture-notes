b64d197c1c6cd885dcdef5fbef30bc3e998289b3
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-03-07 09:44:58 -0500

    added for each exercsies

---
+++ M2/higher-order-fn/for-each.js
@@ -0,0 +1,40 @@
+/**
+ * The .forEach() method's callback function has up to three parameters. What are these parameters?
+ */
+
+/**
+ * Update the code below so that it uses .forEach() instead of a for loop. Inside of printAllComics() you will have to write another, anonymous function.
+ */
+const comics = [
+  { title: 'Calvin & Hobbes', author: 'Bill Watterson', kind: 'print' },
+  { title: 'Zen Pencils', author: 'Gavin Aung Than', kind: 'web' },
+  { title: 'Nancy', author: 'Olivia James', kind: 'print' },
+  { title: 'False Knees', author: 'Joshua Barkman', kind: 'web' },
+];
+
+function printAllComics(comics) {
+  // Write your code here.
+  for (let comic of comics) {
+    console.log(`${comic.title} by ${comic.author}`);
+  }
+}
+
+printAllComics(comics);
+
+/**
+ * How can you update your .forEach() code so that it prints out the following?
+
+1. Calvin & Hobbes" by Bill Watterson
+2. Zen Pencils" by Gavin Aung Than
+3. Nancy" by Olivia James
+4. False Knees" by Joshua Barkman
+*/
+
+/**
+ * How can you update your .forEach() code so that it prints out the following?
+
+(1 out of 4) Calvin & Hobbes" by Bill Watterson
+(2 out of 4) Zen Pencils" by Gavin Aung Than
+(3 out of 4) Nancy" by Olivia James
+(4 out of 4) False Knees" by Joshua Barkman
+*/
