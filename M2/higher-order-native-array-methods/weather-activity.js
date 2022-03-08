f53284fda44fef2e934593be9725eddafb997b8c
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-03-08 09:19:24 -0500

    adds higher order method activity

---
+++ M2/higher-order-native-array-methods/weather-activity.js
@@ -0,0 +1,85 @@
+const weatherData = require("./weather-data.js");
+
+/**
+ * Use built in higher order array methods to answer questions about the seven day weather forecast
+ */
+
+/**
+ * 1. Find all the days that will get warmer than 50 degrees.
+ *
+ * @param {Object[]} forecast
+ * @returns {Object[]} - filtered list containing warm days
+ */
+function daysWarmerThan50(forecast) {}
+
+/**
+ * 2. Get a list of the highs and lows for each day.
+ *
+ * @param {Object[]} forecast
+ * @returns {Number[][]} - An array of arrays of numbers
+ *
+ * Example:
+ *
+ * tempRanges(weatherData) // => [[38, 50], [33, 41], [34, 49], [44, 52], [28, 49], [34, 41], [44, 56]]
+ */
+function tempRanges(forecast) {}
+
+/**
+ * 3. Print out a weather summary for every day in the seven day forecast.
+ *
+ * @param {Object[]} forecast
+ * @returns {Undefined}
+ *
+ * Example:
+ *
+ * logWeatherSummary(weatherData)
+ * // => "Today there is a high of 50 with a 1% chance of rain"
+ * // => ...
+ * // => "Today there is a high of 56 with a 2% chance of rain"
+ */
+function logWeatherSummary(forecast) {}
+
+/**
+ * 4. Find the first day this week that it might snow
+ */
+function findSnowDay(forecast) {}
+
+/**
+ * 5. Get a list of days of the week that the wind will be above 15 mph.
+ *
+ * @param {Object[]} forecast
+ * @returns {String[]} - an array of days of the week with high wind
+ *
+ * Example:
+ *
+ * highWindDays(weatherData) // => ["Tuesday", "Saturday", "Sunday"]
+ */
+function highWindDays(forecast) {}
+
+/**
+ * 6. Print out the low temp for days with less than a 5% chance of precipitation.
+ *
+ * @param {Object[]} forecast
+ * @returns {Undefined}
+ *
+ * Example:
+ *
+ * logSunnyDayLows(weatherData)
+ * // => "3/8/2022 has a low of 38"
+ * // => "3/13/2022 has a low of 34"
+ * // => "3/14/2022 has a low of 44"
+ */
+function logSunnyDayLows(forecast) {}
+
+/**
+ * 7. Given the current temparature, find the first day of the week it could plausibly be
+ *
+ * @param {Object[]} forecast
+ * @param {Number} temperature
+ * @returns {String || Null} - the earliest date it could plausibly be
+ *
+ * Example:
+ *
+ * findDayByTemp(weatherData, 51) // => "3/11/2022"
+ */
+function findDayByTemp(forecast, temperature) {}
---
+++ M2/higher-order-native-array-methods/weather-data.js
@@ -0,0 +1,93 @@
+module.exports = [
+  {
+    date: "3/8/2022",
+    highTemp: 50,
+    lowTemp: 38,
+    wind: {
+      speed: 17,
+      direction: "NW",
+    },
+    precipitation: {
+      chance: 0.01,
+      type: "rain",
+    },
+  },
+  {
+    date: "3/9/2022",
+    highTemp: 41,
+    lowTemp: 33,
+    wind: {
+      speed: 7,
+      direction: "ENE",
+    },
+    precipitation: {
+      chance: 0.98,
+      type: "rain, snow",
+    },
+  },
+  {
+    date: "3/10/2022",
+    highTemp: 49,
+    lowTemp: 34,
+    wind: {
+      speed: 4,
+      direction: "NW",
+    },
+    precipitation: {
+      chance: 0.06,
+      type: "rain",
+    },
+  },
+  {
+    date: "3/11/2022",
+    highTemp: 52,
+    lowTemp: 44,
+    wind: {
+      speed: 7,
+      direction: "ESE",
+    },
+    precipitation: {
+      chance: 0.06,
+      type: "rain",
+    },
+  },
+  {
+    date: "3/12/2022",
+    highTemp: 49,
+    lowTemp: 28,
+    wind: {
+      speed: 19,
+      direction: "WNW",
+    },
+    precipitation: {
+      chance: 0.93,
+      type: "rain, snow",
+    },
+  },
+  {
+    date: "3/13/2022",
+    highTemp: 41,
+    lowTemp: 34,
+    wind: {
+      speed: 16,
+      direction: "W",
+    },
+    precipitation: {
+      chance: 0.0,
+      type: "rain",
+    },
+  },
+  {
+    date: "3/14/2022",
+    highTemp: 56,
+    lowTemp: 44,
+    wind: {
+      speed: 11,
+      direction: "SW",
+    },
+    precipitation: {
+      chance: 0.02,
+      type: "rain",
+    },
+  },
+];
