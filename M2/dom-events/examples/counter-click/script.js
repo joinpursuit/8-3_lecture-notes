9340daa8869ad179f701009d67bd3e3b4df8b690
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-03-29 11:18:07 -0400

    refactor click counter example

--- M2/dom-events/examples/counter-click/script.js
+++ M2/dom-events/examples/counter-click/script.js
@@ -5,20 +5,18 @@ const clickMeP = document.querySelector('p');
 // verifying we've accessed the correct tag
 console.log(clickMeP.textContent);
 
-const increment = (count) => {
-  return count + 1;
+const incrementTextContent = () => {
+  console.log("i've been clicked!!");
+  // how can I add to the click count?
+  // we parse the string text content as a integer
+  let count = parseInt(span.textContent);
+  // increment the count
+  count++;
+  // convert the integer count back to string
+  const countText = count.toString();
+  // update the span value
+  span.textContent = countText;
 };
 
 // use an event listener to create a behavior for clicking the tag
-clickMeP.addEventListener('click', () => {
-  console.log("i've been clicked!!");
-  // how can I add to the click count?
-  // we parse the string content as a integer
-  let count = parseInt(span.textContent);
-  // incremen the count
-  count = increment(count);
-  // convert the integer count back to string
-  const spanCount = count.toString();
-  // update the span value
-  span.textContent = spanCount;
-});
+clickMeP.addEventListener('click', incrementTextContent);
