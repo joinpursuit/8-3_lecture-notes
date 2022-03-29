2aca940962c528e32ce22cbec7f215fb6b0a3c64
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-03-29 09:13:26 -0400

    added events notes

---
+++ M2/events/examples/generating-list-items/script.js
@@ -0,0 +1,28 @@
+/*
+  Comment out the code below before commenting in the working code that follows.
+*/
+const lis = document.querySelectorAll("li");
+for (let li of lis) {
+  li.addEventListener("click", () => {
+    const ol = document.querySelector("ol");
+    const newLi = document.createElement("li");
+    newLi.textContent = "Click me to make more!";
+    ol.append(newLi);
+  });
+}
+
+/*
+  Comment in the following code after commenting out the code above for a working version.
+*/
+// const lis = document.querySelectorAll("li");
+// function generateLiHandler () {
+//   const ol = document.querySelector("ol");
+//   const newLi = document.createElement("li");
+//   newLi.textContent = "Click me to make more!";
+//   ol.append(newLi);
+//   newLi.addEventListener("click", generateLiHandler);
+// }
+
+// for (let li of lis) {
+//   li.addEventListener("click", generateLiHandler);
+// }
---
+++ M2/events/examples/multiple-listeners/script.js
@@ -0,0 +1,9 @@
+let count = 0;
+const span = document.querySelector("h1 span");
+const buttons = document.querySelectorAll("button");
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    count++;
+    span.textContent = count;
+  });
+}
