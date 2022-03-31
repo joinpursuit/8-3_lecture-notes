ba86c4a80515c9168ba8ea6bddb0cbe4d38a062f
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-03-31 14:19:57 -0400

    added form events example

--- M2/dom-events/examples/form/script.js
+++ M2/dom-events/examples/form/script.js
@@ -3,4 +3,19 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log('submitted');
+
+  const author = event.target.author.value;
+  const isbn = document.querySelector('#isbn');
+
+  const genres = event.target.genres;
+
+  const isComedy = document.querySelector('#comedy').checked;
+
+  if (isComedy) {
+    event.target.author.style.textDecoration = 'line-through';
+  } else {
+    event.target.author.style.textDecoration = 'none';
+  }
+
+  console.log(author, isbn, genres, isComedy);
 });
