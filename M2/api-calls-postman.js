6478f3b9768d86be10d9e6d5c76abc923e425ae8
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-03-31 10:13:48 -0400

    added api call postman notes

---
+++ M2/api-calls-postman.js
@@ -0,0 +1,5 @@
+// Recall what you previously learned about the request-response cycle. Related to that, what is a client?
+// How is a client different than a server?
+// What is a web API?
+// What is JSON?
+// What does it mean for a server to be a JSON web API?
