bf405c341378c9c4c0c0a66a347a6d8a71f97e12
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-04-04 09:49:32 -0400

    renamed and removed boilerplate from activities

---
+++ M2/api-calls-fetch/script.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://randomuser.me/api';
+
+// Fetch data from base url using fetch and promises
+
+// create a function to display dom elements from the api response
+
+// create a function to display errors from the api response
