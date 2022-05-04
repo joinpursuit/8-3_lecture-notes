e05f1cb778eba175d64b8c81474c2084e4744e1c
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-05-04 10:17:22 -0400

    added notes react intro

--- M3/intro-to-react/calculator/calculator-object.js
+++ M3/intro-to-react/calculator/calculator-object.js
@@ -1,6 +1,7 @@
 // potential data structure for the calculator app: https://ahfarmer.github.io/calculator/
 const calcButtons = {
   currentValue: 0,
+  digits: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   operations: [
     {
       value: '÷',
@@ -23,7 +24,6 @@ const calcButtons = {
       operation: '=',
     },
   ],
-  numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   actions: [
     {
       value: 'ac',
---
+++ M3/intro-to-react/intro-to-react.js
@@ -0,0 +1,18 @@
+/**
+ * npm
+ *  What does npm init do?
+ *  Describe the purpose of the package.json file
+ *  How can you create scripts via the package.json file?
+ *
+ * npx
+ *  What does the create-react-app package do?
+ *  What do the export, import, and default do?
+ *
+ * What is JSX?
+ *
+ * What are React Elements?
+ *
+ * What are React Components?
+ *
+ * What is the relationship between the React Virtual DOM and the DOM?
+ */
