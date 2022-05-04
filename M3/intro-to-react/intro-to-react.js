ee2604d5f5ae5e107ec7f23952af645d53976962
a-n-g-i-e-r-i <tristan.angieri@gmail.com>
2022-05-04 11:09:27 -0400

    added notes react

--- M3/intro-to-react/intro-to-react.js
+++ M3/intro-to-react/intro-to-react.js
@@ -1,18 +1,46 @@
 /**
  * npm
- *  What does npm init do?
- *  Describe the purpose of the package.json file
- *  How can you create scripts via the package.json file?
+ *  Node Package Manager: A package is similar to a library -- useful reference helper methods to facilitate building an app on top of. Calculus app makes use of a calculus library so that the dev doesn't have to reinvent the wheel.
+ *    - React is a NPM package
+ *      - Harder to break
+ *      - React is designed to write front ends faster, and for them to be more efficient.
  *
- * npx
- *  What does the create-react-app package do?
- *  What do the export, import, and default do?
+ *  What does `npm init` do?
+ *    Simlar to `git init` It sets up a new or existing npm package/project.
+ *  Describe the purpose of the package.json file?
+ *
+ *  How can you create scripts via the package.json file?
+ *    The heart/manifest/map of any node project, the configuration (config)
+ *       - records metadata
+ *          - title, author, licenses, the main script, etc.
+ *          - scripts
+ *             - tests
+ *             - running the app
+ *          - about publishing the package
+ *          - manages dependencies and versions
+ *
+ * npx: Node Package Execute -- temporarily installs a package, and executes a package function
+ *  - Do you have to run npm init before running npx?
+ *    - No, but you must have npm and npx installed
+ *      - When you install Node, npm and npx are also installed
+ *  - What does the create-react-app package do?
+ *    - `npx create-react-app my-app`
+ *      - Temporarily installs react, and creates a new react app
+ *
+ *  - What do the export, import, and default do?
+ *    - Export keyword allows making a function or set of functions available to other files in an npm project
+ *    - import allows importing other functions into a specific file
+ *    - export default keywords combined signify that a single function is being made available from a single file
  *
  * What is JSX?
  *
  * What are React Elements?
  *
  * What are React Components?
+ *  Does each component in react needs its own js file? That's a good practice!
+ *  Can we use react to build a better weather app? YES!
+ *  Will we be viewing a walkthrough of installing react?
+ *  Is all modern web design done with either React or Angular?
  *
  * What is the relationship between the React Virtual DOM and the DOM?
  */
